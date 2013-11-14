52db45a702fdd9ccb380019bd74ad18a63d1bb6c
Sara Gorecki <sgorecki@gmail.com>
2013-11-14 14:43:06 -0500

    add secret and gsub to encoder

--- encoder.rb
+++ encoder.rb
@@ -1,1 +1,4 @@
 secret = "Sometimes, I like to stuff M&M's in my mouth just to see how many I can fit."
+
+#SomeSimes, I like So sSuff M&M's in my mouSh jusS So see how many I can fiS.
+sub1 = secret.gsub("t",secret[0])
