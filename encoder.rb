81dce00000295f0980ecd060ef95c766307d673f
Sara Gorecki <sgorecki@gmail.com>
2013-11-14 15:19:00 -0500

    add random numbers into encoder and update resulting message in decoder

--- encoder.rb
+++ encoder.rb
@@ -2,3 +2,16 @@ secret = "Sometimes, I like to stuff M&M's in my mouth just to see how many I ca
 
 #SomeSimes, I like So sSuff M&M's in my mouSh jusS So see how many I can fiS.
 sub1 = secret.gsub("t",secret[0])
+
+#random = sub1.each_char{ |c| c rand(10).to_s}
+
+sub2 = sub1
+
+i = 0
+while i < sub2.length
+	if i%2 == 0
+		sub2.insert(i, rand(10).to_s)
+	end
+	i += 1
+end
+puts sub2
